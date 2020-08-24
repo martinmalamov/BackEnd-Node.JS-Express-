d00191f8a6de304a201b96155d06304932545880
martinmalamov <47690979+martinmalamov@users.noreply.github.com>
2020-08-24 21:22:54 +0300

    connect with db

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseUrl: `mongodb+srv://user:${process.env.DB_PASSWORD}@cluster0.t7dpb.mongodb.net/cubicle?retryWrites=true&w=majority`
     },
     production: {}
 };
